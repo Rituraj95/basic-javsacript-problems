5a7e84ba698163fc93a5f8353a248350881e6a03
Rituraj95 <96946078+Rituraj95@users.noreply.github.com>
2023-11-06 18:56:42 +0530

    Create area-of-triangle.js

---
+++ area-of-triangle.js
@@ -0,0 +1,7 @@
+const basevalue = prompt("enter the value of base");
+const heightvalue= prompt("enter the value of height ");
+
+const area= (basevalue*heightvalue)/2;
+console.log(
+  `The area of the triangle is ${area}`
+);
