043785bc2932ec2a275e5a1ec3af48fe73a9dd70
Rituraj95 <96946078+Rituraj95@users.noreply.github.com>
2023-11-06 19:02:41 +0530

    Create check-positive-negetive.js

---
+++ check-positive-negetive.js
@@ -0,0 +1,10 @@
+const num =parseInt(prompt('enter the no:'));
+if(num>0){
+    console.log("the no is positive");
+}
+else if(num<0){
+    console.log("the no is negetive");
+}
+else{
+    console.log("the no is negetive");
+}
