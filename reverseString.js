5ca69d105ca435aaf0dd6bc48c24778dc4b1ce77
Rituraj95 <96946078+Rituraj95@users.noreply.github.com>
2023-11-08 20:42:58 +0530

    Create reverseString.js

---
+++ reverseString.js
@@ -0,0 +1,13 @@
+function reverseString(str) {
+    
+    var splitString = str.split(""); 
+    
+    var reverseArray = splitString.reverse(); 
+   
+    var joinArray = reverseArray.join(""); 
+    
+    
+    return joinArray; 
+}
+ 
+reverseString("hello");
