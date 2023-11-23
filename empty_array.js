017a5a2efde949859d1f2a8fc48092fe0a15aa47
Rituraj95 <96946078+Rituraj95@users.noreply.github.com>
2023-11-23 23:33:31 +0530

    Create empty_array.js

---
+++ empty_array.js
@@ -0,0 +1,16 @@
+
+
+function emptyArray(arr) {
+
+   
+    arr = [];
+    
+    return arr;
+}
+
+const array = [1, 2 ,3];
+console.log(array);
+
+
+const result = emptyArray(array);
+console.log(result);
